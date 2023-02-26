688b8ab6d492d6f7360000dde9880c814ce1154d
Roy Ali Hasan <royalihasan0987@gmail.com>
2023-02-26 18:38:12 +0500

    Changes in Search Page

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "../styles/SearchPage.css";
+import TuneOutlinedIcon from "@mui/icons-material/TuneOutlined";
 
 function SearchPage() {
-  return <div className="SearchPage">SearchPage</div>;
+  return (
+    <div className="SearchPage">
+      <div className="searchPage__filter">
+        <TuneOutlinedIcon />
+        <h2>FILTER</h2>
+      </div>
+      <hr />
+    </div>
+  );
 }
 
 export default SearchPage;
